cd8c694d4ca7f656b12806709c202b710a4db7c1
TONYA JENNINGS <you@example.com>
2017-07-29 18:33:59 -0400

    coin changers mob complete

--- coin_changer_mob.rb
+++ coin_changer_mob.rb
@@ -6,6 +6,8 @@ def make_change(amount)
 			exchange[coin] = amount / value
 			amount = amount % value
 		end
+	
+#Removed the following code to simplify code so that you do not repeat same code to do the same thing.  You end up itterating through the hash to generate the correct coins
 	# if amount >= coins["dime"]
 	# 	exchange["dime"] = amount / coins["dime"]
 	# 	amount = amount % coins["dime"]
