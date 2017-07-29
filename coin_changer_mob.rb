b87aeeeb139c3cc92c8a4d75585f87660d8329e3
TONYA JENNINGS <you@example.com>
2017-07-29 16:57:42 -0400

    8 cent fail

--- coin_changer_mob.rb
+++ coin_changer_mob.rb
@@ -1,7 +1,10 @@
 def make_change(amount)
 	if amount >= 5
-		{"nickel" => amount / 5}	
-	elsif amount >= 1
+		{"nickel" => amount / 5}
+		amount = amount - 5	
+	end
+
+	if amount >= 1
 	{"penny" => amount}
 	else
 		{}
