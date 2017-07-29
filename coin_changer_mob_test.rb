cb5f249f801fe6e848d4eea2688532531fd6d55c
TONYA JENNINGS <you@example.com>
2017-07-29 16:28:58 -0400

    penny = 1

--- coin_changer_mob_test.rb
+++ coin_changer_mob_test.rb
@@ -11,4 +11,8 @@ class TestCoin_Changer_Mob < Minitest::Test
 		assert_equal({}, make_change(0))
 	end	
 
+	def test_assert_that_1_equals_1_penny
+		assert_equal({"penny" => 1}, make_change(1))
+	end
+
 end
