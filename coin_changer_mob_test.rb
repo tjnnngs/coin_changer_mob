dca9c4b484a4859160725c3e535c1668628c1d0e
TONYA JENNINGS <you@example.com>
2017-07-29 16:20:30 -0400

    test 2

--- coin_changer_mob_test.rb
+++ coin_changer_mob_test.rb
@@ -6,4 +6,9 @@ class TestCoin_Changer_Mob < Minitest::Test
 	def test_assert_that_1_equals_1
 		assert_equal(1, 1)
 	end
+
+	def test_assert_that_0_equals_empty_hash
+		assert_({}), make_change(0))
+	end	
+
 end
