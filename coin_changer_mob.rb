614f2590a5b70e8eb51b4945b6086fa4eb3773bf
TONYA JENNINGS <you@example.com>
2017-07-29 17:38:11 -0400

    8 cent error still

--- coin_changer_mob.rb
+++ coin_changer_mob.rb
@@ -1,13 +1,12 @@
 def make_change(amount)
+	exchange = {}
 	if amount >= 5
-		{"nickel" => amount / 5}
-		amount = amount - 5	
+		exchange["nickel"] = amount / 5
+	end
+	if amount >= 1
+		exchange["penny"] = amount
 	end
 
-	if amount >= 1
-	{"penny" => amount}
-	else
-		{}
-	end
+exchange
 end
 
